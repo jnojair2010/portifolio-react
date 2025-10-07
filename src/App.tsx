5361b11a873b24bd17d5dcaf1c5a19147bf92760
jair do Nascimento de Oliveira <jnojair2010@gmail.com>
2025-10-07 06:29:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Menu from './components-views/menu'
+
+function App() {
+
+
+  return (
+    <div className='body-app'>
+      <Menu />
+
+    </div>
+  )
+}
+
+export default App
